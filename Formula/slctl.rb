f4bdda1e9ad6e7980e3c8352be0c1a4031e1d0a3
Shih Yu Ho <methodho@gmail.com>
2018-11-21 11:39:33 +0800

    Create slctl.rb

---
+++ Formula/slctl.rb
@@ -0,0 +1,17 @@
+class slctl < Formula
+  desc "Slctl is a command line interface for running commands against SoftLeader Services"
+  homepage "https://github.com/softleader/slctl"
+  url "https://github.com/softleader/slctl/releases/download/0.1.0/slctl-macos-.tgz"
+  version "0.1.0"
+  
+  depends_on "git"
+
+  def install
+    bin.install "slctl"
+  end
+
+  def caveats; <<~EOS
+    To begin working with slctl, run the 'slctl init' command.
+  EOS
+  end
+end
