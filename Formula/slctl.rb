414c7675800de5f16cfae2d93f30979598a840f5
Shih Yu Ho <methodho@gmail.com>
2018-12-27 23:25:23 +0800

    Update slctl.rb

--- Formula/slctl.rb
+++ Formula/slctl.rb
@@ -1,8 +1,8 @@
 class Slctl < Formula
   desc "Slctl is a command line interface for running commands against SoftLeader Services"
   homepage "https://github.com/softleader/slctl"
-  url "https://github.com/softleader/homebrew-tap/releases/download/slctl/slctl-macos.tgz"
-  version "0.1.0"
+  url "https://github.com/softleader/homebrew-tap/releases/download/slctl/slctl-darwin-0.1.8.tgz"
+  version "0.1.8"
   
   depends_on "git"
 
