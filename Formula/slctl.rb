5c4b413a9f5015b25760ffd28deff2dfab02f175
softleader/homebrew-tap/brew-tapper <supprt@softleader.com.tw>
2019-01-02 12:31:08 +0800

    version upgrade by brew-tapper bot

--- Formula/slctl.rb
+++ Formula/slctl.rb
@@ -1,7 +1,7 @@
 class Slctl < Formula
   desc "Slctl is a command line interface for running commands against SoftLeader Services"
   homepage "https://github.com/softleader/slctl"
-  version "0.1.8"
+  version "0.2.0"
   
   if OS.mac?
     url "https://github.com/softleader/slctl/releases/download/#{version}/slctl-darwin-#{version}.tgz"
