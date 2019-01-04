3e3d0e05fcc1ccff5bba4e5f6c904d678e6d18be
Shih Yu Ho <methodho@gmail.com>
2019-01-04 15:39:55 +0800

    Update slctl.rb

--- Formula/slctl.rb
+++ Formula/slctl.rb
@@ -5,8 +5,10 @@ class Slctl < Formula
   
   if OS.mac?
     url "https://github.com/softleader/slctl/releases/download/#{version}/slctl-darwin-#{version}.tgz"
+    sha256 "49ac626a3b3356a3b16bb422b33df284fe4dbafa5dba6473cde2e519aabd5183"
   elsif OS.linux?
     url "https://github.com/softleader/slctl/releases/download/#{version}/slctl-linux-#{version}.tgz"
+    sha256 "f45b086701887633d0b2a237c03d1a8171a6a54fd80b9092cfe0682fbfb5e72e"
   end
 
   depends_on :arch => :x86_64
