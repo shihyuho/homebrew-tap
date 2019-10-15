da9e8bb14dac161900628884fb8da29730fb2cb7
softleader/homebrew-tap/brew-tapper <supprt@softleader.com.tw>
2019-10-15 15:46:31 +0800

    version upgrade by brew-tapper bot

--- Formula/slctl.rb
+++ Formula/slctl.rb
@@ -1,14 +1,14 @@
 class Slctl < Formula
   desc "Slctl is a command line interface for running commands against SoftLeader Services"
   homepage "https://github.com/softleader/slctl"
-  version "3.7.5"
+  version "3.8.0"
   
   if OS.mac?
     url "https://github.com/softleader/slctl/releases/download/#{version}/slctl-darwin-#{version}.tgz"
-    sha256 "7a8d7a19bc63dfd65a7ad7f7aeaab2015810c9fd161e860a93a34a3041db5830"
+    sha256 "643433856f1613aaec8f8d3a2fbf6aadfb0a10c423ce697d01c4c2bd2bf3dabe"
   elsif OS.linux?
     url "https://github.com/softleader/slctl/releases/download/#{version}/slctl-linux-#{version}.tgz"
-    sha256 "690c40603f6c7b01bc75dbf000d169546e89ae391ae3315758847b6917be03a0"
+    sha256 "7290d23180859f89a6a57f20b0312a637c5d4978036d9857a8f2941eb420fad3"
   end
 
   depends_on :arch => :x86_64
