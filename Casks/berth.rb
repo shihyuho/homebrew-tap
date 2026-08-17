6c6c1c0980638f0beb698f959c9fe10041ea1908
Shihyu Ho <methodho@gmail.com>
2026-08-17 19:44:22 +0800

    chore(berth): use stable release asset

--- Casks/berth.rb
+++ Casks/berth.rb
@@ -2,7 +2,7 @@ cask "berth" do
   version "1.0.0"
   sha256 "3e5fa16021939cdbddbbee4c73bdc768fad2d79b21107fbbd29ec5b98d39b5ad"
 
-  url "https://github.com/shihyuho/berth/releases/download/#{version}/Berth-#{version}-arm64.zip"
+  url "https://github.com/shihyuho/berth/releases/download/#{version}/Berth-arm64.zip"
   name "Berth"
   desc "Keep your macOS Dock on the display you choose"
   homepage "https://github.com/shihyuho/berth"
